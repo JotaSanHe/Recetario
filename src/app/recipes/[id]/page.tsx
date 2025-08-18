e4f159863ab208fe1f7f550b8901b5bccdbabd92
Jota Sanchis <jsanchishermita@gmail.com>
2025-08-18 22:52:38 +0200

    Solución para error de tipado en Vercel

--- src/app/recipes/[id]/page.tsx
+++ src/app/recipes/[id]/page.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 import { Recipes } from '@/data/recipes';
+import type { PageProps } from "next";
 
-export default function RecipePage({ params }: { params: { id: string } }) {
+export default function RecipePage({ params }: PageProps<{ id: string }>) {
     const receta = Recipes.find(r => r.id === params.id);
 
     if (!receta) {
